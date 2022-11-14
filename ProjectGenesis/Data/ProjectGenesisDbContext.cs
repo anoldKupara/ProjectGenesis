6211b98e1aeff7e9c16f948e4c767c8df1f5ae1c
Anold Kupara <kuparaanold20@gmail.com>
2022-11-14 15:25:04 +0200

    added the db context

---
+++ ProjectGenesis/Data/ProjectGenesisDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectGenesis.Data
+{
+    public class ProjectGenesisDbContext : DbContext
+    {
+        public ProjectGenesisDbContext(DbContextOptions<ProjectGenesisDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectGenesis.Models.Project> Project { get; set; }
+    }
+}
