e012df222b44aec1b38151b9fd947524a8f56e45
Anold Kupara <kuparaanold20@gmail.com>
2022-11-17 10:34:52 +0200

    added BirdCategoriesController

---
+++ ProjectGenesis/Controllers/BirdCategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectGenesis.Data;
+
+namespace ProjectGenesis.Controllers
+{
+    public class BirdCategoriesController : Controller
+    {
+        private readonly ProjectGenesisDbContext _projectGenesisDb;
+
+        public BirdCategoriesController(ProjectGenesisDbContext projectGenesisDb)
+        {
+            _projectGenesisDb = projectGenesisDb;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
