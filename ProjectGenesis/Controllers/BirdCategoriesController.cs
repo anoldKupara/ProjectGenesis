c6aa1aeb23add71b9c0e231c3a0a54a4e7f58069
Anold Kupara <kuparaanold20@gmail.com>
2022-11-17 10:43:15 +0200

    added code for creating a Bird Category

--- ProjectGenesis/Controllers/BirdCategoriesController.cs
+++ ProjectGenesis/Controllers/BirdCategoriesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProjectGenesis.Data;
+using ProjectGenesis.Models.Entities;
+using ProjectGenesis.Models.ViewModels.BirdCategories;
 
 namespace ProjectGenesis.Controllers
 {
@@ -14,6 +16,33 @@ namespace ProjectGenesis.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateBirdCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBirdCategory(AddBirdCategoryViewModel addBirdCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var birdCategory = new BirdCategory
+                {
+                    Name = addBirdCategoryViewModel.Name,
+                    Purpose = addBirdCategoryViewModel.Purpose,
+                    Breed = addBirdCategoryViewModel.Breed
+                };
+
+                _projectGenesisDb.BirdCategories.Add(birdCategory);
+                await _projectGenesisDb.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(addBirdCategoryViewModel);
         }
     }
 }
