36ef2eccc162f92fbb6dc6fe25623df7375056d1
Anold Kupara <kuparaanold20@gmail.com>
2022-11-18 12:11:14 +0200

    created BudgetController

---
+++ ProjectGenesis/Controllers/BudgetController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectGenesis.Data;
+
+namespace ProjectGenesis.Controllers
+{
+    public class BudgetController : Controller
+    {
+        private readonly ProjectGenesisDbContext _projectGenesisDbContext;
+
+        public BudgetController(ProjectGenesisDbContext projectGenesisDbContext)
+        {
+           _projectGenesisDbContext = projectGenesisDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
