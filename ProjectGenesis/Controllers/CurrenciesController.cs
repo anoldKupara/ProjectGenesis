acfa5235df51e3744db1239a36d5917b3fd4a051
Anold Kupara <kuparaanold20@gmail.com>
2022-11-18 15:09:49 +0200

    added code for creating a currency

--- ProjectGenesis/Controllers/CurrenciesController.cs
+++ ProjectGenesis/Controllers/CurrenciesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProjectGenesis.Data;
+using ProjectGenesis.Models.Entities;
+using ProjectGenesis.Models.ViewModels.Currencies;
 
 namespace ProjectGenesis.Controllers
 {
@@ -17,6 +19,32 @@ namespace ProjectGenesis.Controllers
         {
             var currencies = await _projectGenesisDbContext.Currencies.ToListAsync();
             return View(currencies);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateCurrency()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCurrency(AddCurrencyViewModel addCurrencyViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var currency = new Currency
+                {
+                    Name = addCurrencyViewModel.Name,
+                    Symbol = addCurrencyViewModel.Symbol
+                };
+
+                _projectGenesisDbContext.Currencies.Add(currency);
+                await _projectGenesisDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(addCurrencyViewModel);
         }
     }
 }
