65285e7503ef7f538af647121368aeef1e686b64
Anold Kupara <kuparaanold20@gmail.com>
2022-11-18 13:11:40 +0200

    added edit functionality for the budgets

--- ProjectGenesis/Controllers/BudgetsController.cs
+++ ProjectGenesis/Controllers/BudgetsController.cs
@@ -53,5 +53,55 @@ namespace ProjectGenesis.Controllers
             return View(addBudgetViewModel);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> EditBudget(Guid id)
+        {
+            var budget = await _projectGenesisDbContext.Budgets.FindAsync(id);
+            if (budget == null)
+            {
+                return NotFound();
+            }
+
+            var editBudgetViewModel = new EditBudgetViewModel
+            {
+                Name = budget.Name,
+                Purpose = budget.Purpose,
+                SourceOfFund = budget.SourceOfFund,
+                ProjectStartDate = budget.ProjectStartDate,
+                ProjectEndDate = budget.ProjectEndDate,
+                Amount = budget.Amount,
+                Currency = budget.Currency
+            };
+
+            return View(editBudgetViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditBudget(Guid id, EditBudgetViewModel editBudgetViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var budget = await _projectGenesisDbContext.Budgets.FindAsync(id);
+                if (budget == null)
+                {
+                    return NotFound();
+                }
+
+                budget.Name = editBudgetViewModel.Name;
+                budget.Purpose = editBudgetViewModel.Purpose;
+                budget.SourceOfFund = editBudgetViewModel.SourceOfFund;
+                budget.ProjectStartDate = editBudgetViewModel.ProjectStartDate;
+                budget.ProjectEndDate = editBudgetViewModel.ProjectEndDate;
+                budget.Amount = editBudgetViewModel.Amount;
+                budget.Currency = editBudgetViewModel.Currency;
+
+                await _projectGenesisDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(editBudgetViewModel);
+        }
+
     }
 }
