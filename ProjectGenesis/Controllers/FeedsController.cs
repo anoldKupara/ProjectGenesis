170c4660731c54c254e44c36441a607797d557ac
Anold Kupara <kuparaanold20@gmail.com>
2022-11-18 16:11:13 +0200

    added FeedsController

---
+++ ProjectGenesis/Controllers/FeedsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectGenesis.Data;
+
+namespace ProjectGenesis.Controllers
+{
+    public class FeedsController : Controller
+    {
+        private readonly ProjectGenesisDbContext _projectGenesisDbContext;
+
+        public FeedsController(ProjectGenesisDbContext projectGenesisDbContext)
+        {
+            _projectGenesisDbContext = projectGenesisDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
