dae79c5b185021a88827c970b8bd857a8b274dcb
Anold Kupara <kuparaanold20@gmail.com>
2022-11-18 15:58:24 +0200

    added code for recording an expense

--- ProjectGenesis/Controllers/ExpensesController.cs
+++ ProjectGenesis/Controllers/ExpensesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProjectGenesis.Data;
+using ProjectGenesis.Models.Entities;
+using ProjectGenesis.Models.ViewModels.Expenses;
 
 namespace ProjectGenesis.Controllers
 {
@@ -16,6 +18,36 @@ namespace ProjectGenesis.Controllers
         {
             var expenses = await _projectGenesisDbContext.Expenses.ToListAsync();
             return View(expenses);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateExpense()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateExpense(AddExpenseViewModel addExpenseViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var expense = new Expense
+                {
+                    Name = addExpenseViewModel.Name,
+                    Purpose = addExpenseViewModel.Purpose,
+                    SourceOfFund = addExpenseViewModel.SourceOfFund,
+                    ExpenseDate = addExpenseViewModel.ExpenseDate,
+                    Amount = addExpenseViewModel.Amount,
+                    Currency = addExpenseViewModel.Currency
+                };
+
+                _projectGenesisDbContext.Expenses.Add(expense);
+                await _projectGenesisDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(addExpenseViewModel);
         }
     }
 }
