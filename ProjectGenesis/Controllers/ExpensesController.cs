44c3460353aba3aeb2c035f2a7addbc488463d8d
Anold Kupara <kuparaanold20@gmail.com>
2022-11-18 16:01:07 +0200

    added edit functionality for the expenses

--- ProjectGenesis/Controllers/ExpensesController.cs
+++ ProjectGenesis/Controllers/ExpensesController.cs
@@ -49,5 +49,55 @@ namespace ProjectGenesis.Controllers
 
             return View(addExpenseViewModel);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> EditExpense(Guid id)
+        {
+            var expense = await _projectGenesisDbContext.Expenses.FindAsync(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            var editExpenseViewModel = new EditExpenseViewModel
+            {
+                Id = expense.Id,
+                Name = expense.Name,
+                Purpose = expense.Purpose,
+                SourceOfFund = expense.SourceOfFund,
+                ExpenseDate = expense.ExpenseDate,
+                Amount = expense.Amount,
+                Currency = expense.Currency
+            };
+
+            return View(editExpenseViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditExpense(EditExpenseViewModel editExpenseViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var expense = await _projectGenesisDbContext.Expenses.FindAsync(editExpenseViewModel.Id);
+                if (expense == null)
+                {
+                    return NotFound();
+                }
+
+                expense.Name = editExpenseViewModel.Name;
+                expense.Purpose = editExpenseViewModel.Purpose;
+                expense.SourceOfFund = editExpenseViewModel.SourceOfFund;
+                expense.ExpenseDate = editExpenseViewModel.ExpenseDate;
+                expense.Amount = editExpenseViewModel.Amount;
+                expense.Currency = editExpenseViewModel.Currency;
+
+                _projectGenesisDbContext.Expenses.Update(expense);
+                await _projectGenesisDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(editExpenseViewModel);
+        }
     }
 }
