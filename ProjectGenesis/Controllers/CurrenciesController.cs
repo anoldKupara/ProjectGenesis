fbfa84b7b61818d28f43d2e6f48eb6c1a3a3b4a7
Anold Kupara <kuparaanold20@gmail.com>
2022-11-18 15:06:11 +0200

    added code for displaying all currencies

--- ProjectGenesis/Controllers/CurrenciesController.cs
+++ ProjectGenesis/Controllers/CurrenciesController.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjectGenesis.Data;
 
 namespace ProjectGenesis.Controllers
 {
     public class CurrenciesController : Controller
     {
+        private readonly ProjectGenesisDbContext _projectGenesisDbContext;
+
         public CurrenciesController(ProjectGenesisDbContext projectGenesisDbContext)
         {
-            
+            _projectGenesisDbContext = projectGenesisDbContext;
         }
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var currencies = await _projectGenesisDbContext.Currencies.ToListAsync();
+            return View(currencies);
         }
     }
 }
