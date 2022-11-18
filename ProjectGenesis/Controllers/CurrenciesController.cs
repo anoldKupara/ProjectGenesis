51e2d40c730b829597894c6fdde394eb15bb9b11
Anold Kupara <kuparaanold20@gmail.com>
2022-11-18 15:02:39 +0200

    added CurrenciesController

---
+++ ProjectGenesis/Controllers/CurrenciesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectGenesis.Data;
+
+namespace ProjectGenesis.Controllers
+{
+    public class CurrenciesController : Controller
+    {
+        public CurrenciesController(ProjectGenesisDbContext projectGenesisDbContext)
+        {
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
