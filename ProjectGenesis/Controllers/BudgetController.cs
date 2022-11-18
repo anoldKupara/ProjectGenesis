5e23017b0c503b698509c212acec6f8921c27fc3
Anold Kupara <kuparaanold20@gmail.com>
2022-11-18 12:12:27 +0200

    added code for listing all budgets

--- ProjectGenesis/Controllers/BudgetController.cs
+++ ProjectGenesis/Controllers/BudgetController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjectGenesis.Data;
 
 namespace ProjectGenesis.Controllers
@@ -11,9 +12,14 @@ namespace ProjectGenesis.Controllers
         {
            _projectGenesisDbContext = projectGenesisDbContext;
         }
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var budgets = await _projectGenesisDbContext.Budgets.ToListAsync();
+            return View(budgets);
+            
         }
+        
+        
     }
 }
