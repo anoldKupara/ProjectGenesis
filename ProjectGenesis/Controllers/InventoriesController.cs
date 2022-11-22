afdbb46c79fee3a79b8ddf7deb8b31992cc49845
Anold Kupara <kuparaanold20@gmail.com>
2022-11-22 15:10:29 +0200

    added code to list every inventory availble

--- ProjectGenesis/Controllers/InventoriesController.cs
+++ ProjectGenesis/Controllers/InventoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjectGenesis.Data;
 
 namespace ProjectGenesis.Controllers
@@ -11,9 +12,13 @@ namespace ProjectGenesis.Controllers
         {
             _projectGenesisDbContext = projectGenesisDbContext;
         }
-        public IActionResult Index()
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var inventories = await _projectGenesisDbContext.Inventories.ToListAsync();
+
+            return View(inventories);
         }
     }
 }
