df4ef1ea1142fa3c70932e2579230bd73f4475dc
Anold Kupara <kuparaanold20@gmail.com>
2022-11-22 14:59:55 +0200

    created the InventoriesController

---
+++ ProjectGenesis/Controllers/InventoriesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectGenesis.Data;
+
+namespace ProjectGenesis.Controllers
+{
+    public class InventoriesController : Controller
+    {
+        private readonly ProjectGenesisDbContext _projectGenesisDbContext;
+
+        public InventoriesController(ProjectGenesisDbContext projectGenesisDbContext)
+        {
+            _projectGenesisDbContext = projectGenesisDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
