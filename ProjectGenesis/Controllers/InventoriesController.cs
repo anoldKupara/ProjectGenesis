15c92ec604739a5e411e6954900aa4ca623ed1c1
Anold Kupara <kuparaanold20@gmail.com>
2022-11-22 15:19:52 +0200

    added functionality to record a new inventory

--- ProjectGenesis/Controllers/InventoriesController.cs
+++ ProjectGenesis/Controllers/InventoriesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProjectGenesis.Data;
+using ProjectGenesis.Models.Entities;
+using ProjectGenesis.Models.ViewModels.Inventories;
 
 namespace ProjectGenesis.Controllers
 {
@@ -19,6 +21,36 @@ namespace ProjectGenesis.Controllers
             var inventories = await _projectGenesisDbContext.Inventories.ToListAsync();
 
             return View(inventories);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateInventory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateInventory(AddInventoryViewModel addInventoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var inventory = new Inventory
+                {
+                    ItemName = addInventoryViewModel.ItemName,
+                    Purpose = addInventoryViewModel.Purpose,
+                    Quantity = addInventoryViewModel.Quantity,
+                    Amount = addInventoryViewModel.Amount,
+                    Currency = addInventoryViewModel.Currency,
+                    ExpiryDate = addInventoryViewModel.ExpiryDate
+                };
+
+                _projectGenesisDbContext.Inventories.Add(inventory);
+                await _projectGenesisDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(addInventoryViewModel);
         }
     }
 }
