610b60b3197c1fe842c92cb5a82df792a844a933
Anold Kupara <kuparaanold20@gmail.com>
2022-11-23 08:36:08 +0200

    Added the PaymentMethodsController

---
+++ ProjectGenesis/Controllers/PaymentMethodsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectGenesis.Data;
+
+namespace ProjectGenesis.Controllers
+{
+    public class PaymentMethodsController : Controller
+    {
+        private readonly ProjectGenesisDbContext _projectGenesisDbContext;
+        public PaymentMethodsController(ProjectGenesisDbContext projectGenesisDbContext)
+        {
+            _projectGenesisDbContext = projectGenesisDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
