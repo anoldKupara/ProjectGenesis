01a4024f2e19cb09e8b10e58c19eedd07f7b3fe1
Anold Kupara <kuparaanold20@gmail.com>
2022-11-23 09:20:53 +0200

    added edit functionality for the PaymentMethods

--- ProjectGenesis/Controllers/PaymentMethodsController.cs
+++ ProjectGenesis/Controllers/PaymentMethodsController.cs
@@ -47,5 +47,49 @@ namespace ProjectGenesis.Controllers
 
             return View(addPaymentMethodViewModel);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> EditPaymentMethod(Guid id)
+        {
+            var paymentMethod = await _projectGenesisDbContext.PaymentMethods.FindAsync(id);
+
+            if (paymentMethod == null)
+            {
+                return NotFound();
+            }
+
+            var editPaymentMethodViewModel = new EditPaymentMethodViewModel
+            {
+                Id = paymentMethod.Id,
+                Name = paymentMethod.Name,
+                Currency = paymentMethod.Currency
+            };
+
+            return View(editPaymentMethodViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditPaymentMethod(EditPaymentMethodViewModel editPaymentMethodViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var paymentMethod = await _projectGenesisDbContext.PaymentMethods.FindAsync(editPaymentMethodViewModel.Id);
+
+                if (paymentMethod == null)
+                {
+                    return NotFound();
+                }
+
+                paymentMethod.Name = editPaymentMethodViewModel.Name;
+                paymentMethod.Currency = editPaymentMethodViewModel.Currency;
+
+                _projectGenesisDbContext.PaymentMethods.Update(paymentMethod);
+                await _projectGenesisDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(editPaymentMethodViewModel);
+        }
     }
 }
