db28a3a76943c47c6cfb918d20efd314d2a6cde1
Anold Kupara <kuparaanold20@gmail.com>
2022-11-23 09:11:03 +0200

    added code for displaying all the Payment Methods

--- ProjectGenesis/Controllers/PaymentMethodsController.cs
+++ ProjectGenesis/Controllers/PaymentMethodsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjectGenesis.Data;
+using ProjectGenesis.Models.Entities;
+using ProjectGenesis.Models.ViewModels.PaymentMethods;
 
 namespace ProjectGenesis.Controllers
 {
@@ -10,9 +13,39 @@ namespace ProjectGenesis.Controllers
         {
             _projectGenesisDbContext = projectGenesisDbContext;
         }
-        public IActionResult Index()
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var paymentMethods = await _projectGenesisDbContext.PaymentMethods.ToListAsync();
+
+            return View(paymentMethods);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreatePaymentMethod()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePaymentMethod(AddPaymentMethodViewModel addPaymentMethodViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var paymentMethod = new PaymentMethod
+                {
+                    Name = addPaymentMethodViewModel.Name,
+                    Currency = addPaymentMethodViewModel.Currency
+                };
+
+                _projectGenesisDbContext.PaymentMethods.Add(paymentMethod);
+                await _projectGenesisDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(addPaymentMethodViewModel);
         }
     }
 }
