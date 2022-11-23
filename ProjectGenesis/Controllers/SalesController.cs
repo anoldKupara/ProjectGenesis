f189c5742601a24f0183aa2dc6bdecafbf96b4e5
Anold Kupara <kuparaanold20@gmail.com>
2022-11-23 14:11:40 +0200

    added edit functionality for a sale

--- ProjectGenesis/Controllers/SalesController.cs
+++ ProjectGenesis/Controllers/SalesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProjectGenesis.Data;
+using ProjectGenesis.Models.Entities;
+using ProjectGenesis.Models.ViewModels.Sales;
 
 namespace ProjectGenesis.Controllers
 {
@@ -19,5 +21,83 @@ namespace ProjectGenesis.Controllers
             var sales = await _projectGenesisDbContext.Sales.ToListAsync();
             return View(sales);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateSale()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSale(AddSaleViewModel addSaleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var sale = new Sale
+                {
+                    ItemPurchased = addSaleViewModel.ItemPurchased,
+                    Quantity = addSaleViewModel.Quantity,
+                    Price = addSaleViewModel.Price,
+                    PaymentMethod = addSaleViewModel.PaymentMethod,
+                    DateOfPurchase = addSaleViewModel.DateOfPurchase
+                };
+
+                _projectGenesisDbContext.Sales.Add(sale);
+                await _projectGenesisDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(addSaleViewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditSale(Guid id)
+        {
+            var sale = await _projectGenesisDbContext.Sales.FindAsync(id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+
+            var editSaleViewModel = new EditSaleViewModel
+            {
+                Id = sale.Id,
+                ItemPurchased = sale.ItemPurchased,
+                Quantity = sale.Quantity,
+                Price = sale.Price,
+                PaymentMethod = sale.PaymentMethod,
+                DateOfPurchase = sale.DateOfPurchase
+            };
+
+            return View(editSaleViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditSale(EditSaleViewModel editSaleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var sale = await _projectGenesisDbContext.Sales.FindAsync(editSaleViewModel.Id);
+                if (sale == null)
+                {
+                    return NotFound();
+                }
+
+                sale.ItemPurchased = editSaleViewModel.ItemPurchased;
+                sale.Quantity = editSaleViewModel.Quantity;
+                sale.Price = editSaleViewModel.Price;
+                sale.PaymentMethod = editSaleViewModel.PaymentMethod;
+                sale.DateOfPurchase = editSaleViewModel.DateOfPurchase;
+
+                await _projectGenesisDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(editSaleViewModel);
+        }
+        
+        
     }
 }
