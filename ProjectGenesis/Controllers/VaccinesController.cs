86cc39e2c756c891168a937bab8b748773a0b11d
Anold Kupara <kuparaanold20@gmail.com>
2022-11-23 14:21:13 +0200

    added the VaccinesController

---
+++ ProjectGenesis/Controllers/VaccinesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectGenesis.Data;
+
+namespace ProjectGenesis.Controllers
+{
+    public class VaccinesController : Controller
+    {
+        private readonly ProjectGenesisDbContext _projectGenesisDbContext;
+
+        public VaccinesController(ProjectGenesisDbContext projectGenesisDbContext)
+        {
+            _projectGenesisDbContext = projectGenesisDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
